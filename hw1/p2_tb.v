109a47e6157221d504075c0b8c5baae1a7c05cb8
Ben Scholar <bbs27@case.edu>
2022-09-12 21:59:59 -0400

    cleaned up p2

--- hw1/p2_tb.v
+++ hw1/p2_tb.v
@@ -11,13 +11,13 @@ module p2_tb;
   
   // outputs
   wire [2*N - 1:0] p;
-  wire co, valid;
+  wire valid;
 
   // modules
   mult2v2 mult(
     .clk(clk), .load(load),
     .a(a), .b(b),
-    .p(p), .cout(co),
+    .p(p),
     .valid(valid)
   );
 
@@ -26,29 +26,29 @@ module p2_tb;
   always #10 clk = !clk;
 
   initial begin 
-    $monitor("a=%0d, b=%0d => p=%0d, co=%0b", a, b, p, co);
+    $monitor("a=%0d, b=%0d => p=%0d, valid=%0b", a, b, p, valid);
 
     a = 4'd2;
     b = 4'd4;
     load = 1'b1;
     #20
     load = 1'b0;
-
     #100
-
-    a <= 4'd3;
-    b <= 4'd15;
-    load <= 1'b1;
+  
+    $display("");
+    a = 4'd3;
+    b = 4'd15;
+    load = 1'b1;
     #20
-    load <=1'b0;
-
+    load = 1'b0;
     #100
 
+    $display("");
     a = 4'd15;
     b = 4'd15;
-    load <= 1'b1;
+    load = 1'b1;
     #20
-    load <= 1'b0;
+    load = 1'b0;
 
     #100
 
